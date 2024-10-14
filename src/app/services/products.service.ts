4f9e45f6136db9cb9c21fbeaaf90badf0e3388d6
AdolfoGit <adolfoangelhernandezmanuel@gmail.com>
2024-10-14 10:24:27 -0600

    CDMM-17 listado de productos

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Products } from '../interface/products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getALLProducts(){
+    const path="https://lacasadelmariscoweb.azurewebsites.net/api/CasaDelMarisco/TraerProductos";
+    return this.http.get<Products[]>(path);
+  }
+
 }
