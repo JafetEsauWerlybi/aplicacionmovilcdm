06b0c7c1f388fd0a11b6e41620424373f13b38a1
JafetEsauWerlybi <jeckooficial@gmail.com>
2024-10-25 12:25:40 -0600

    asd;lfas;ld

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:8100', 
+    baseUrl: 'http://localhost:8100', 
     setupNodeEvents(on, config) {
     },
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}', 
