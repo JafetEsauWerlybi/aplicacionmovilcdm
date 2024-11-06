d676b235cce666775e28b9c3d542ba7e4c892077
JafetEsauWerlybi <jeckooficial@gmail.com>
2024-11-06 17:45:19 -0600

    malditas pruebas jsjsj

--- cypress/e2e/productos.cy.ts
+++ cypress/e2e/productos.cy.ts
@@ -14,72 +14,84 @@ describe('Tab2Page - Pruebas de la página de productos', () => {
         Icono: 'https://example.com/icon.png',
       };
   
-      // Configura el usuario cada vez que se visita la página
       const angular = (win as any).ng;
       const appComponent = angular.getComponent(win.document.querySelector('app-tab2'));
       appComponent.setUserDataForTesting(userData);
   
-      // Espera para asegurarse de que los datos se actualicen en la vista
       cy.wait(500);
     });
   };
 
   beforeEach(() => {
+    cy.viewport('iphone-x'); // o cualquier otro tamaño de dispositivo móvil
     setUser();
   });
 
   it('Debería cargar los productos en la página', () => {
-    cy.contains('Fresh Tomato', { timeout: 10000 }).should('be.visible');
+    cy.contains('Fresh Tomato', { timeout: 10000 })
+      .scrollIntoView({ offset: { top: -window.innerHeight / 2, left: 0 }, easing: 'linear', duration: 500 })
+      .should('be.visible');
     console.log('Productos cargados correctamente.');
   });
 
   it('Debería abrir el modal de detalles del producto y permitir agregar al carrito', () => {
-    setUser();  // Configura el usuario nuevamente
+    setUser();
+    cy.wait(1000);
     cy.get('ion-button#open-product-modal', { timeout: 10000 })
       .should('exist')
       .first()
-      .scrollIntoView()
+      .scrollIntoView({ offset: { top: -window.innerHeight / 2, left: 0 }, easing: 'linear', duration: 500 })
+      .should('be.visible')
       .click({ force: true });
 
-    cy.get('ion-modal', { timeout: 10000 })
-      .should('be.visible');
+    cy.get('ion-modal', { timeout: 10000 }).should('be.visible');
 
+    cy.wait(1000);
     cy.contains('Agregar al carrito', { timeout: 10000 })
-      .scrollIntoView()
+      .scrollIntoView({ offset: { top: -window.innerHeight / 2, left: 0 }, easing: 'linear', duration: 500 })
       .should('be.visible')
-      .click();
+      .click({ force: true });
 
     console.log('Modal de detalles del producto abierto correctamente.');
   });
 
   it('Debería de mostrar idUsuario', () => {
-    setUser();  // Configura el usuario nuevamente
-    cy.contains('Jafet Esaú ', { timeout: 10000 }).should('be.visible');
-    console.log('Usuario parceado correctamente.');
+    setUser();
+    cy.wait(1000);
+    cy.contains('Jafet Esaú', { timeout: 10000 })
+      .scrollIntoView({ offset: { top: -window.innerHeight / 2, left: 0 }, easing: 'linear', duration: 500 })
+      .should('be.visible');
+    console.log('Usuario parseado correctamente.');
   });
 
   it('Debería de agregar idUsuario', () => {
-    setUser();  // Configura el usuario nuevamente
+    setUser();
+    cy.wait(1000);
     cy.get('ion-button#open-product-modal', { timeout: 10000 })
       .should('exist')
       .first()
-      .scrollIntoView()
+      .scrollIntoView({ offset: { top: -window.innerHeight / 2, left: 0 }, easing: 'linear', duration: 500 })
+      .should('be.visible')
       .click({ force: true });
 
     cy.get('ion-modal', { timeout: 10000 }).should('be.visible');
 
+    cy.wait(1000);
     cy.contains('Agregar al carrito', { timeout: 10000 })
-      .scrollIntoView()
+      .scrollIntoView({ offset: { top: -window.innerHeight / 2, left: 0 }, easing: 'linear', duration: 500 })
       .should('be.visible')
-      .click({force : true});
+      .click({ force: true });
 
     cy.wait(1000);
 
     cy.get('ion-alert').should('exist');
-    cy.contains('Producto agregado').should('be.visible');
+    cy.contains('Producto agregado')
+      .scrollIntoView({ offset: { top: -window.innerHeight / 2, left: 0 }, easing: 'linear', duration: 500 })
+      .should('be.visible');
 
     cy.get('ion-alert button')
       .contains('Aceptar')
-      .click();
+      .scrollIntoView({ offset: { top: -window.innerHeight / 2, left: 0 }, easing: 'linear', duration: 500 })
+      .click({ force: true });
   });
 });
